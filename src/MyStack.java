ca4bc53ed9086ed018b1426661c115932f4e210e
rose <jasmineun07@gmail.com>
2024-01-13 00:15:29 +0900

    Initial commit

---
+++ src/MyStack.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class MyStack<T> {
+
+    private LinkedList<T> list;
+
+    public MyStack() {
+        list = new LinkedList<>();
+
+    }
+
+    public void add(T data) {
+        list.add(data);
+    }
+
+    public T get() {
+        return list.pop(); // peek, pop : 가져오면서 삭제
+    }
+}
